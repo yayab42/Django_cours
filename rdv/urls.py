cb7e64d4450fa325b7ac9c0eebef410b31360562
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-11-29 14:23:17 +0100

    feat: login page

---
+++ rdv/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import LogoutView, LoginView
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('index/', views.index, name='index'),
+]
