01538d894e5178aa913d4e1f572048de2a1ed730
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-11-29 14:23:54 +0100

    login pages

--- project_0/settings.py
+++ project_0/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rdv.apps.RdvConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index/'
