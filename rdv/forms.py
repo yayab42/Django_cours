5fef0460462047d679e3fb897c86ab6a63b7b5b8
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-11-30 09:11:40 +0100

    Add base with header and footer, bootstrap

--- rdv/forms.py
+++ rdv/forms.py
@@ -1,9 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from rdv.models import Rdv
+from django import forms
 
 
 # Create the form class.
 class RdvForm(ModelForm):
+    date: forms.DateTimeField(widget=forms.DateTimeInput)
+
     class Meta:
         model = Rdv
         fields = ['date', 'doctor', 'type', 'duration']
